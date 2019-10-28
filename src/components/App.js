6cef7f109bdf94e106d7683f528dc5e8ddfd0589
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2019-10-28 17:49:00 -0500

    switching machines

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import '../css/styles.css';
+import '../css/App.css';
 
 class App extends React.Component {
     render() {
         return (
-            <div></div>
+            <div className="App"></div>
         )
     }
 }
